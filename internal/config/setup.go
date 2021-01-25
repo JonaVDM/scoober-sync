2de11c36b83914bc1f2fe86e8b9fa821ee5195a8
Jonathan <jonajvdm@gmail.com>
2021-01-25 15:54:51 +0100

    Added google sign in

--- internal/config/setup.go
+++ internal/config/setup.go
@@ -1,13 +1,24 @@
 package config
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/jonavdm/scoober-sync/internal/scoober"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
 )
 
 // Setup will run through all the setup steps
 func Setup() error {
+	gtk, err := setupGoogle()
+	if err != nil {
+		return err
+	}
+
 	scb, err := setupScoober()
 	if err != nil {
 		return err
@@ -15,6 +26,7 @@ func Setup() error {
 
 	config := Config{
 		ScooberToken: scb,
+		GoogleToken:  gtk,
 	}
 
 	err = config.Save()
@@ -25,8 +37,29 @@ func Setup() error {
 	return nil
 }
 
-func setupGoogle() {
+func setupGoogle() (*oauth2.Token, error) {
+	path := os.Getenv("SCOOBER_CONFIG")
+	b, err := ioutil.ReadFile(path + "/credentials.json")
+	if err != nil {
+		return nil, err
+	}
 
+	config, err := google.ConfigFromJSON(b, calendar.CalendarEventsScope)
+	if err != nil {
+		return nil, err
+	}
+
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		return nil, err
+	}
+
+	tok, err := config.Exchange(context.TODO(), code)
+	return tok, err
 }
 
 func setupCalendar() {
--- internal/config/type.go
+++ internal/config/type.go
@@ -1,6 +1,9 @@
 package config
 
+import "golang.org/x/oauth2"
+
 // Config holds all the applications config
 type Config struct {
 	ScooberToken string
+	GoogleToken  *oauth2.Token
 }
