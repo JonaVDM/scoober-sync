093af5a7d22f27f345ef3742b0cdba764ba6e202
Jonathan <jonajvdm@gmail.com>
2021-01-29 17:16:37 +0100

    Added logging

--- cmd/scoober-sync/sync.go
+++ cmd/scoober-sync/sync.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
+	"github.com/jonavdm/scoober-sync/internal/config"
+	"github.com/jonavdm/scoober-sync/internal/discord"
 	"github.com/jonavdm/scoober-sync/internal/sync"
 )
 
@@ -12,5 +16,39 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Print(changes)
+	conf, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var messages []string
+
+	for _, added := range changes.Added {
+		messages = append(messages, fmt.Sprintf("Added %s", added))
+	}
+
+	for _, deleted := range changes.Deleted {
+		messages = append(messages, fmt.Sprintf("Removed %s", deleted))
+	}
+
+	for _, updated := range changes.Updated {
+		messages = append(messages, fmt.Sprintf("Updated %s", updated))
+	}
+
+	msg := strings.Join(messages, "\n")
+
+	if msg == "" {
+		log.Println("No changed have been made")
+		return
+	}
+
+	log.Println(msg)
+
+	if conf.DiscordHook != "" {
+		err = discord.SendWebhook(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Send message to webhook")
+	}
 }
